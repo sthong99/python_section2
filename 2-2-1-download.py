7342aac86e1c4a3a903c2d75169faad568e6606a
durm7 <sthong99@naver.com>
2018-04-06 13:52:17 +0900

    두번째 수정 v1.2

--- 2-2-1-download.py
+++ 2-2-1-download.py
@@ -5,6 +5,8 @@ import urllib.request as dw
 sys.stdout = io.TextIOWrapper(sys.stdout.detach(), encoding = 'utf-8')
 sys.stderr = io.TextIOWrapper(sys.stderr.detach(), encoding = 'utf-8')
 
+print('hi')
+
 imgUrl ="http://post.phinf.naver.net/20160621_169/1466482468068lmSHj_JPEG/If7GeIbOPZuYwI-GI3xU7ENRrlfI.jpg"
 htmlURL ="http://google.com"
 
